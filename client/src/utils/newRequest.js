c4c81ef3962611343340ed3c8e303fe1835ba2d4
Aditya Pratap Singh <138649643+s-aditya-dev@users.noreply.github.com>
2024-07-04 22:24:42 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   // baseURL: "http://localhost:8000/api/",
-  baseURL: "rudra-backend-git-main-adityas-projects-a46e79b7.vercel.app/",
+  baseURL: "rudra-backend-git-main-adityas-projects-a46e79b7.vercel.app/api/",
   withCredentials: true,
 });
 
