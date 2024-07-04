c2c88d24f7e11eb5bb8574402054253b10053857
Aditya Pratap Singh <138649643+s-aditya-dev@users.noreply.github.com>
2024-07-04 22:22:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/user": "https://rudra-backend.vercel.app/",
+      "/user": "rudra-backend-git-main-adityas-projects-a46e79b7.vercel.app/",
     },
   },
   plugins: [react()],
