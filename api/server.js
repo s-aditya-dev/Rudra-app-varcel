5b80fa202f30c528b7c8eed10e2d71117c70cfee
Aditya Pratap Singh <138649643+s-aditya-dev@users.noreply.github.com>
2024-07-04 22:32:40 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -24,21 +24,7 @@ const connect = async () => {
   }
 };
 
-const allowedOrigins = ["https://rudra-app-varcel.onrender.com"];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 app.use(
   cors({
