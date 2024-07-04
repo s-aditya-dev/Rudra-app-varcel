7baf63519674604513050a323687306d9dfbe776
Aditya <sraditya1203@gmail.com>
2024-07-04 20:17:12 +0530

    request changes

--- api/server.js
+++ api/server.js
@@ -24,13 +24,22 @@ const connect = async () => {
   }
 };
 
-app.use(
-  cors({
-    // origin: "http://localhost:5173",
-    origin: "https://rudra-main.vercel.app/",
-    credentials: true,
-  })
-);
+const allowedOrigins = ["https://rudra-main.vercel.app"];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
