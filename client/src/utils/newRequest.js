2bce40da83c0dbb8518677d9c6ef70357553f8a8
Aditya <sraditya1203@gmail.com>
2024-07-04 21:16:42 +0530

    request update v2

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   // baseURL: "http://localhost:8000/api/",
-  baseURL: "https://rudra-app-varcel.vercel.app/",
+  baseURL: "https://rudra-backend.vercel.app/",
   withCredentials: true,
 });
 
